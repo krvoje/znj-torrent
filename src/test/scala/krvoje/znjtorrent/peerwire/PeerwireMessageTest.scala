ff2f1bf5c0c5d545256977dced960ba693eb148b
krvoje <hrvoje.peradin@oradian.com>
2017-08-28 08:09:42 +0200

    Add deserialization and tests

---
+++ src/test/scala/krvoje/znjtorrent/peerwire/PeerwireMessageTest.scala
@@ -0,0 +1,39 @@
+package krvoje.znjtorrent.peerwire
+
+import org.specs2._
+
+class PeerwireMessageTest extends mutable.Specification {
+
+  "Serialization roundtrip" >> {
+    rt(Handshake(SHA1("0123456789ABCDEFGHIJ"), PeerID("0123456789ABCDEFGHIJ")))
+    rt(Keepalive())
+    rt(Choke())
+    rt(Unchoke())
+    rt(Interested())
+    rt(NotInterested())
+    rt(Have(Index(10)))
+    rt(Bitfield(false, false, false))
+    rt(Request(10, 2, 11)) // TODO: Invalid
+    rt(Piece(10, 2, Array[Byte](1, 1))) // TODO: Invalid
+    rt(Cancel(10, 2, 11)) // TODO: Invalid
+    rt(Port(PortValue(9000)))
+  }
+
+  private def rt(msg: PeerwireMessage) = {
+    s"$msg" >> {
+      "Serialize -> Deserialize" >> { Peerwire.deserialize(msg.ser) ==== padBools(msg) }
+      "Ser - > Deser -> Ser" >> {Peerwire.deserialize(msg.ser).ser ==== msg.ser}
+    }
+  }
+
+  private def padBools(msg: PeerwireMessage): PeerwireMessage = msg match {
+    case Bitfield(pl) => Bitfield(padBools(pl))
+    case _ => msg
+  }
+
+  private def padBools(bs: Array[Boolean]): Array[Boolean] = {
+    val toPad = (bs.length % Bitfield.BYTE_SIZE - Bitfield.BYTE_SIZE).abs
+    bs.padTo(bs.length + toPad, false)
+  }
+
+}
